199b958fec4d1f1c083636dee83eaeef7b160de8
chen_anguo <cag2050@163.com>
2018-11-14 15:34:04 +0800

    entry_output

---
+++ 1_entry_output/README.md
@@ -0,0 +1,3 @@
+* webpack 命令默认：`webpack`等价于`webpack --config webpack.config.js` 或 `webpack --config webpackfile.js`
+
+* 不存在这2个配置文件： webpack.config.js、webpackfile.js，也可以直接运行`webpack`命令。此时，会以默认值构建。
---
+++ 1_entry_output/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // 默认值
+    /*
+    entry: './src/index3.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    }
+    */
+};
